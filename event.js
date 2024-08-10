15d4581643ea66834a3867dc2dd28c0af3d3d3a8
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-10 11:25:42 +0530

    event and functions in js

---
+++ event.js
@@ -0,0 +1,17 @@
+// Events in JavaScript
+// Events are specific types of situations, and in these situations, we can perform specific types of work. JavaScript is an event-handling programming language that supports various types of events. Some basic events are:
+
+// onclick
+// onmouseover
+// onsubmit
+// onfocus
+// onblur
+// onchange
+// onmouseout
+// Functions in JavaScript
+
+// Functions are small programs or subprograms. We can create functions in JavaScript using the function keyword followed by the function name.
+
+function functionName() {
+    // Body of the function
+  }
